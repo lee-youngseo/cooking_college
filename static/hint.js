1528512947d0ba20ac29eea624cee99f72c2534b
20leo02 <lrm2188@columbia.edu>
2024-05-02 21:06:19 -0400

    Add recipe redirect button and initial styling.

--- static/hint.js
+++ static/hint.js
@@ -1,11 +1,15 @@
-function displayHint(hintID, hint){
+function displayHint(){
     let hints = [displayHintOne(), displayHintTwo(), displayHintThree(), displayHintFour(), displayHintFive()];
     $('.subtitle').append(`${hint['name']}`);
+    $('#recipe-redirect').append(`Go to recipe`);
     $('#hint-container').append(hints[hintID]);
 }
 
 function displayHintOne(){
-
+    let tipOne = $(`<div class='tip'></div>`);
+    let tipTwo = $(`<div class='tip'></div>`);
+    let tipThree = $(`<div class='tip'></div>`);
+    let steps = hint['steps'];
 }
 
 function displayHintTwo(){
@@ -25,6 +29,11 @@ function displayHintFive(){
 }
 
 $(function(){
-    displayHint(hintID, hint)
+    displayHint()
+
+    $('#recipe-redirect').on('click', function(){
+        window.location.href = `/learn_recipe/${hint['recipe_id']}/instructions`;
+
+    })
 
 })
